7266d9f67b2c3dd48d6016b0270d89e8a4cba605
Kimutai <jankimsingoei@gmail.com>
2023-10-20 16:12:02 +0300

    Progress

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, make_response
 from flask_migrate import Migrate
+import traceback
 
 from models import db, Zookeeper, Enclosure, Animal
 
@@ -19,15 +20,57 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    if not animal:
+        response_body = '<h1>404 Animal not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+
+    response_body = f'''
+        <ul>ID: {animal.id}</ul>
+        <ul>Name: {animal.name}</ul>
+        <ul>Species: {animal.species}</ul>
+    '''
+
+    response = make_response(response_body, 200)
+
+    return response
+       
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    if not zookeeper:
+        response_body = '<h1>404 Zookeeper not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+
+    response_body = f'''
+        <ul>Name {zookeeper.name}</ul>
+        <ul>Birthday {zookeeper.birthday}</ul>
+    '''
+
+    response = make_response(response_body, 200)
+
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    if not enclosure:
+        response_body = '<h1>404 Enclosure not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+
+    response_body = f'''
+        <ul>ID: {enclosure.id}</ul>
+        <ul>Environment: {enclosure.environment}</ul>
+        <ul>Open to Visitors: {enclosure.open_to_visitors}</ul>
+    '''
+
+    response = make_response(response_body, 200)
+
+    return response
 
 
 if __name__ == '__main__':
